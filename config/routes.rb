a26613152fd4e84656f190ed11f8a42b91547c2d
bennay <benny.tseng.is@gmail.com>
2016-08-09 14:49:58 -0700

    BLOG

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  get '/' => 'home#home', as: :home
+  get '/about' => 'home#about', as: :about
+  root 'home#home'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
